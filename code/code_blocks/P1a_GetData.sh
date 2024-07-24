0f1083825bdabd41461cfbd1364644fe214c2146
Evan Peikon <154470963+evanpeikon@users.noreply.github.com>
2024-07-24 08:39:30 -0400

    Update P1a_GetData.sh

--- code/code_blocks/P1a_GetData.sh
+++ code/code_blocks/P1a_GetData.sh
@@ -1,2 +1,7 @@
 wget -O GSE95077_Normalized_Count_Matrix_JJN3_Amiloride_and_CTRL.txt.gz 'https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE95077&format=file&file=GSE95077%5FNormalized%5FCount%5FMatrix%5FJJN3%5FAmiloride%5Fand%5FCTRL%2Etxt%2Egz'
 gunzip GSE95077_Normalized_Count_Matrix_JJN3_Amiloride_and_CTRL.txt
+
+python3 #switch to python environment 
+path = 'GSE95077_Normalized_Count_Matrix_JJN3_Amiloride_and_CTRL.txt'
+data = pd.read_csv(path, sep='\t', index_col=0)
+data.head()
