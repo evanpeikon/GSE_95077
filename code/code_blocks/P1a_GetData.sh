a6ea29b7a95b2d38eb4a5af7bd41fcd8004fbdc0
Evan Peikon <154470963+evanpeikon@users.noreply.github.com>
2024-07-24 08:38:28 -0400

    Create P1a_GetData.sh

---
+++ code/code_blocks/P1a_GetData.sh
@@ -0,0 +1,2 @@
+wget -O GSE95077_Normalized_Count_Matrix_JJN3_Amiloride_and_CTRL.txt.gz 'https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE95077&format=file&file=GSE95077%5FNormalized%5FCount%5FMatrix%5FJJN3%5FAmiloride%5Fand%5FCTRL%2Etxt%2Egz'
+gunzip GSE95077_Normalized_Count_Matrix_JJN3_Amiloride_and_CTRL.txt
