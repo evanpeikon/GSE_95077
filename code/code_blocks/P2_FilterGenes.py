21174d8a944fd2200c852b5c19fa816b90553251
Evan Peikon <154470963+evanpeikon@users.noreply.github.com>
2024-08-19 10:23:19 -0400

    Delete code/code_blocks directory

--- code/code_blocks/P2_FilterGenes.py
+++
@@ -1,7 +0,0 @@
-# create function to normalize and filter genes 
-def filter_genes(data, min_cpm=1, min_samples=3):
-    cpm = data.apply(lambda x: (x / x.sum()) * 1e6) 
-    mask = (cpm > min_cpm).sum(axis=1) >= min_samples 
-    return data[mask]
-# filter genes 
-data = filter_genes(data)
